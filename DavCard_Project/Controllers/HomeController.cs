c4fe4a50123c6ecb976da1d4fe49189f63a941b9
Nishtman_Coding <Farid.physic2024@gmail.com>
2024-10-25 01:17:40 -0700

    Upload temple

---
+++ DavCard_Project/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DavCard_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DevCard_MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public HomeController()
+		{
+
+
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();	
+		}
+
+
+		[ResponseCache(Duration =0 , Location = ResponseCacheLocation.None , NoStore =true   ) ]
+		public IActionResult Error()
+		{
+
+			return View(new ErrorViewModel { RequestId=Activity.Current?.Id ?? HttpContext.TraceIdentifier }   );
+		}
+
+	}
+}
